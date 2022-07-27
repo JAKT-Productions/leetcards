df2a9a3be180b9938bde68957396aebdb892ccd5
MockTech <83368864+MockTech@users.noreply.github.com>
2022-07-27 11:56:36 -0400

    FEStructure bones

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 // Import components and pages
+import DashboardView from './Views/DashboardView';
+import LoginView from './Views/LoginView';
+import SignupView from './Views/SignupView';
+import StudyView from './Views/StudyView';
 
 export default function App() {
+
+  const [data, setData] = useState({});
+
+
   return (
-    <div>App</div>
-  )
+    <Routes>
+      <Route path={'/'} element={<LoginView setData={setData}/>} />
+      <Route path="/signup" element={<SignupView />} />
+
+      <Route
+        path="/dashboard/access"
+        element={
+          Object.keys(data).length > 0 ? (
+            <DashboardView data={data} />
+          ) : (
+            'Invalid permissions'
+          )
+        }
+      />
+    </Routes>
+  );
 }
---
+++ client/Views/DashboardView.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+// Import components and pages
+
+export default function DashboardView() {
+
+  return (
+    <></>
+  );
+}
