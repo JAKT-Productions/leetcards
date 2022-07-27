b425b4dd978be865aee54d404662fbb944e7dc9c
Jin Yoo <iyoojin@gmail.com>
2022-07-26 23:33:45 -0400

    added getDecks and getCards controllers

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -2,12 +2,49 @@ const db = require('../models/appModel');
 
 const apiController = {};
 
-apiController.getDeck = async (req, res, next) => {
-    const query = {
-        text: "SELECT" ,
-        value: []
-    }
+apiController.getDecks = async (req, res, next) => {
+  const { user_id } = req.body;
 
-}
+  try {
+    const query = {
+      text: 'SELECT * FROM decks WHERE user_id=$1',
+      values: [user_id],
+    };
+    const data = await db.query(query);
+    const allDecks = data.rows;
+    const userDecks = {};
+    allDecks.map((element) => {
+      userDecks['user_id'] = element.user_id;
+      userDecks[element.deck_name] = element.deck_id;
+    });
+    res.locals.getDecks = userDecks;
+
+    return next();
+  } catch (err) {
+    next({
+      log: 'Express error handler caught error in apiController.getDecks',
+      message: { err: 'An error occurred in requesting decks' },
+    });
+  }
+};
+
+apiController.getCards = async (req, res, next) => {
+  const { deck_id } = req.body;
+
+  try {
+    const query = {
+      text: 'SELECT * FROM cards WHERE deck_id=$1',
+      values: [deck_id],
+    };
+    const data = await db.query(query);
+    res.locals.getCards = data.rows;
+    return next();
+  } catch (err) {
+    next({
+      log: 'Express error handler caught error in apiController.getDecks',
+      message: { err: 'An error occurred in requesting decks' },
+    });
+  }
+};
 
 module.exports = apiController;
--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -2,31 +2,37 @@ const express = require('express');
 
 const router = express.Router();
 
+const apiController = require('../controllers/apiController');
+
 // Getting all the data including user info, decks, cards, tags.
-router.get('/getCards', (req, res) => {
-    console.log('Data Requested');
-    res.status(200).json('hi');  
+router.post('/getDecks', apiController.getDecks, (req, res) => {
+  console.log('Data Requested');
+  res.status(200).json(res.locals.getDecks);
+});
+
+router.post('/getCards', apiController.getCards, (req, res) => {
+  console.log('Data Requested');
+  res.status(200).json(res.locals.getCards);
 });
 
 router.post('/createDeck', (req, res) => {
-    console.log('Deck is created!');
-    res.status(200).json('hi');  
+  console.log('Deck is created!');
+  res.status(200).json('hi');
 });
 
 router.post('/createCard', (req, res) => {
-    console.log('Card is created!');
-    res.status(200).json('hi');  
+  console.log('Card is created!');
+  res.status(200).json('hi');
 });
 
 // router.post('/deleteDeck', (req, res) => {
 //     console.log('Deck is deleted');
-//     res.status(200).json('hi');  
+//     res.status(200).json('hi');
 // });
 
 // router.post('/deleteCard', (req, res) => {
 //     console.log('Card is deleted');
-//     res.status(200).json('hi');  
+//     res.status(200).json('hi');
 // });
-
 
 module.exports = router;
