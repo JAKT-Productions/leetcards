215f914d052ea21d1f46a5614364192c093c5113
Alina Gasperino <alina.gasperino@gmail.com>
2022-07-27 14:09:04 -0400

    Drawer 60%

--- client/Components/DeckDrawer.jsx
+++ client/Components/DeckDrawer.jsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
-<<<<<<< HEAD
-=======
 import { useEffect, useState } from 'react';
->>>>>>> dev
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
@@ -28,15 +25,6 @@ export default function DeckDrawer() {
     right: false,
   });
   
-<<<<<<< HEAD
-  const DeckNames = ['Deck1', 'Deck2', 'Deck3', 'Deck4', 'Deck5'];
-  
-  const addDeck = () => {
-    // Access text field
-    const deckName = document.getElementById('deck-input');
-    console.log(deckName);
-    // console.log("hi");
-=======
   const [DeckNames, setDeckNames] = useState(['Deck1', 'Deck2', 'Deck3', 'Deck4', 'Deck5']);
   const [drawerContents, setDrawerContents] = useState();
 
@@ -52,7 +40,6 @@ export default function DeckDrawer() {
       [...prevDecks, 'Deck6']
     })
     // console.log("hi"); Note, current version doesn't have access to text in input field 1:50pm
->>>>>>> dev
     // If text field is not empty --> submit a POST request to database
     // on success --> add deck to decklist & store response in state --> clear text field
   };
@@ -65,14 +52,11 @@ export default function DeckDrawer() {
   //   setState({ ...state, [anchor]: open });
   // };
 
-<<<<<<< HEAD
-=======
   useEffect(()=> {
     const sidebar = list('left');
     setDrawerContents(sidebar);
   }, [DeckNames]);
   
->>>>>>> dev
   const list = (anchor) => (
     <Box
       sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
@@ -82,39 +66,24 @@ export default function DeckDrawer() {
     >
       Hello
       <Divider />
-<<<<<<< HEAD
-      <Box sx={{ '& > :not(style)': { m: 1 } }}>
-=======
       <Box sx={{ '& > :not(style)': { m: 1 } }} onSubmit={addDeck}>
->>>>>>> dev
         <h2>Decks</h2>
         <>
           <TextField
               id="deck-input"
               label="Add deck"
               type="input"
-<<<<<<< HEAD
-              variant="standard"
-            />
-          <Fab size="small" color="secondary" aria-label="add">
-            <AddIcon onClick={addDeck}/>
-=======
               name="deck-input"
               variant="standard"
             />
           <Fab type="submit" size="small" color="secondary" aria-label="add">
             <AddIcon/>
->>>>>>> dev
           </Fab>
         </>
       </Box>
       <List>
         {[...DeckNames].map((text, index) => (
-<<<<<<< HEAD
-          <ListItem key={text} disablePadding>
-=======
           <ListItem key={text} onClick={console.log("render deck cards logic here")} disablePadding>
->>>>>>> dev
             <ListItemButton>
               <ListItemIcon>
                 {index % 2 === 0 ? <StyleIcon /> : <StyleIcon />}
@@ -138,11 +107,7 @@ export default function DeckDrawer() {
             open={state[anchor]}
             // onClose={toggleDrawer(anchor, false)}
           >
-<<<<<<< HEAD
-            {list(anchor)}
-=======
             {drawerContents}
->>>>>>> dev
           </Drawer>
         </React.Fragment>
       ))}
